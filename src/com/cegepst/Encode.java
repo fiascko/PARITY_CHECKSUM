86b38716dd6c9fbe49fa05c97c15f1c74bed8bfb
fiascko <jeremyethier@outlook.com>
2021-09-26 14:15:53 -0400

    CREATE A TRANSFORM FUN TO CREATE A BINARY STRING

--- src/com/cegepst/Encode.java
+++ src/com/cegepst/Encode.java
@@ -1,9 +1,24 @@
 package com.cegepst;
 
+import java.util.ArrayList;
+
 public class Encode {
 
-    public Encode(String textMessage) {
+    private String binaryString;
 
+    public Encode(String textMessage) {
+        binaryString = transform(textMessage);
+        System.out.println(binaryString);
+    }
+
+    private String transform(String textMessage) {
+        StringBuilder result = new StringBuilder();
+        char[] chars = textMessage.toCharArray();
+        for (char aChar : chars) {
+            result.append(String.format("%8s", Integer.toBinaryString(aChar)).replaceAll(" ", "0")                         // zero pads
+            );
+        }
+        return result.toString();
     }
 
 }
