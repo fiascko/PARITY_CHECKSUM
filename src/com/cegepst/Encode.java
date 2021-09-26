bff997d7722323fbe5781f10602980f4c5171036
fiascko <jeremyethier@outlook.com>
2021-09-26 17:54:29 -0400

    CLEAN CONVERT TO BINARY FUN

--- src/com/cegepst/Encode.java
+++ src/com/cegepst/Encode.java
@@ -1,34 +1,11 @@
 package com.cegepst;
 
-import java.util.ArrayList;
-
 public class Encode {
 
-    private String binaryMessage;
-//    private ArrayList<BinaryLine> binaryLines;
-
     public Encode(String textMessage) {
-        binaryMessage = convertToBinary(textMessage);
-//        System.out.println(binaryMessage);
-
-//        createBlockList(binaryString);
-//        addparity();
-//        systemoutString;
+        BinaryShifter binaryShifter = new BinaryShifter(textMessage);
     }
 
-    private String convertToBinary(String textMessage) {
-        StringBuilder bitsStream = new StringBuilder();
-        char[] chars = textMessage.toCharArray();
-        for (char aChar : chars) {
-            bitsStream.append(String.format("%8s", Integer.toBinaryString(aChar)).replaceAll(" ", "0"));
-        }
-        return bitsStream.toString();
-    }
-//
-//    private void createBlockList(String binaryString) {
-//
-//        binaryString.split()
-//
-//    }
+
 
 }
