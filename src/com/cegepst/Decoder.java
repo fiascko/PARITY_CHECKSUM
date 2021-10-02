f2cbdf34154b1fcdc3391ccb528def993dceb7fb
fiascko <jeremyethier@outlook.com>
2021-10-02 19:04:56 -0400

    STARTING DECODER FUN

--- src/com/cegepst/Decoder.java
+++ src/com/cegepst/Decoder.java
@@ -1,12 +1,41 @@
 package com.cegepst;
 
+import java.util.ArrayList;
+
 public class Decoder {
 
+    private ArrayList<Byte> bytes;
+
     public Decoder(String binaryMessage) {
-//        checkIferror()
-//        siErrorRepare()
-//        effacer chaque parity on byte et la ligne de parity
+        initDecoder();
+        decode(binaryMessage);
+    }
+
+    private void initDecoder() {
+        bytes = new ArrayList<>();
+    }
+
+    private void decode(String binaryMessage) {
+        splitBitsStream(binaryMessage);
+//        check si respect()
+//        checkIferrorDansles parity()
+//        siErrorRepareerreur()
+
+//        effacer chaque parity on byte
+//        effacer la ligne de parity
+
 //        translate() les charByte en char dans la string
 //        Sortir() system out
+
     }
+
+    private void splitBitsStream(String bitsStream) {
+        int byteCounter = bitsStream.length() / 9;
+        String[] splitBits = bitsStream.split("(?<=\\G.{9})");
+        for (int i = 0; i < byteCounter; i++) {
+            bytes.add(new Byte(splitBits[i]));
+        }
+    }
+
+
 }
